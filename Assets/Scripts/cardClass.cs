d15987e99433aa140184ef6df9d840a700121d84
TobiasRisom <trisom19@student.aau.dk>
2023-04-12 12:08:30 +0200

    added cardClass and cardDisplay

---
+++ Assets/Scripts/cardClass.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "new card", menuName ="Card")]
+public class cardClass : ScriptableObject
+{
+    public string cardName;
+    public string cardDescription;
+
+    public Sprite cardDesign;
+
+    public string computerPart1;
+    public string computerPart2;
+    public string computerPart3;
+    public int computerPartPoint1;
+    public int computerPartPoint2;
+    public int computerPartPoint3;
+
+}
---
+++ Assets/Scripts/cardDisplay.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class cardDisplay : MonoBehaviour
+{
+
+    public cardClass card;
+    public Text cardName;
+    public Text cardDescriptionText;
+
+    public Image artworkImageOfCard;
+    public Text part1;
+    public Text part2;
+    public Text part3;
+    public Text point1;
+    public Text point2;
+
+    public Text point3;
+
+
+    void Start()
+    {
+
+        
+        
+    }
+
+
+}
