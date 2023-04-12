46945dbb1e0a787eb773c230df7c4e55634b4ee0
TobiasRisom <trisom19@student.aau.dk>
2023-04-12 10:44:56 +0200

    Base Game ready for logic

---
+++ Assets/Scripts/Card.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Card : MonoBehaviour
+{
+    public bool hasBeenPlayed;
+
+    public int handIndex;
+
+    private GameManager gm;
+
+    private void Start()
+    {
+        gm = FindObjectOfType<GameManager>();
+    }
+
+    private void OnMouseDown()
+    {
+        if(hasBeenPlayed == false)
+        {
+            transform.position += Vector3.up * 2;
+            hasBeenPlayed = true;
+            gm.availableCardSlots[handIndex] = true;
+            Invoke("MoveToDiscard", 1f);
+        }
+    }
+
+    void MoveToDiscard()
+    {
+        gm.discardPile.Add(this);
+        gameObject.SetActive(false);
+        gm.UpdateCardText();
+    }
+}
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,22 +1,60 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
-public class TobiScript : MonoBehaviour
+public class GameManager : MonoBehaviour
 {
 
-    public List<GameObject> hand;
-    public List<GameObject> deck;
+    public List<Card> deck = new List<Card>();
+    public List<Card> discardPile = new List<Card>();
 
-    // Start is called before the first frame update
-    void Start()
+    public Transform[] cardslots;
+    public bool[] availableCardSlots;
+
+    public TextMeshProUGUI deckSizeText;
+    public TextMeshProUGUI discardPileText;
+
+    public void DrawCard()
     {
-        
+        if(deck.Count >= 1)
+        {
+            Card randCard = deck[Random.Range(0, deck.Count)];
+
+            for (int i = 0; i < availableCardSlots.Length; i++)
+            {
+                if(availableCardSlots[i] == true)
+                {
+                    randCard.gameObject.SetActive(true);
+                    randCard.handIndex = i;
+                    randCard.transform.position = cardslots[i].position;
+                    randCard.hasBeenPlayed = false;
+                    availableCardSlots[i] = false;
+                    deck.Remove(randCard);
+                    UpdateCardText();
+                    return;
+                } 
+            }
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Shuffle()
     {
-        
+        if(discardPile.Count >= 1)
+        {
+            foreach(Card card in discardPile)
+            {
+                deck.Add(card);
+            }
+            discardPile.Clear();
+        }
+        UpdateCardText();
+    }
+
+    public void UpdateCardText()
+    {
+        deckSizeText.text = deck.Count.ToString();
+        discardPileText.text = discardPile.Count.ToString();
     }
 }
