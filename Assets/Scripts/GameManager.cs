88eaf141f29441e18ed69f08bda11734b1ac88dc
TobiasRisom <trisom19@student.aau.dk>
2023-04-12 09:39:49 +0200

    added gamemanager, goodbye tobiscript

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class TobiScript : MonoBehaviour
 {
+
+    public List<GameObject> hand;
+    public List<GameObject> deck;
+
     // Start is called before the first frame update
     void Start()
     {
